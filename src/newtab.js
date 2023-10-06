a5629001329b00dbb9eb808ae9f1f1b75fba8200
Chang Jun <52chinaweb@gmail.com>
2023-10-06 13:53:52 +0800

    feat: change storage to indexdb

--- src/newtab.js
+++ src/newtab.js
@@ -1,51 +1,95 @@
 const SBG_KEY = 'EXTEMSIONS-SBG-KEY';
+const SBG_DB_NAME = 'start-db'
+const SBG_DB_NAME_TABLE = 'bg'
+let db = null;
+init()
 
 const uploadImg = document.getElementById('upload-img');
-
 uploadImg.addEventListener('change', uploadImgChange);
 
-getImg().then((result) => {
-  setBodyBg(result[SBG_KEY])
-});
+async function init() {
+  await initStore();
+  const file = await getImg()
+  setBodyBg(file)
+}
 
-async function uploadImgChange(e){
+function initStore() {
+  const request = window.indexedDB.open(SBG_DB_NAME, 1);
+
+  return new Promise((resolve, reject) => {
+    request.onerror = function (event) {
+      console.log('db open error');
+      reject(event)
+    };
+
+    request.onsuccess = function (event) {
+      db = request.result;
+      resolve(event)
+    };
+
+    request.onupgradeneeded = function (event) {
+      db = event.target.result;
+      let objectStore;
+
+      if (!db.objectStoreNames.contains(SBG_DB_NAME_TABLE)) {
+        objectStore = db.createObjectStore(SBG_DB_NAME_TABLE, { autoIncrement: true });
+      }
+    }
+  })
+}
+
+function setImg(file) {
+  let r = db.transaction([SBG_DB_NAME_TABLE], 'readwrite')
+    .objectStore(SBG_DB_NAME_TABLE)
+    .put(file, 1);
+
+  return new Promise((resolve, reject) => {
+    r.onsuccess = function (event) {
+      resolve(event)
+    };
+
+    r.onerror = function (event) {
+      reject(event)
+    }
+  })
+}
+
+function getImg() {
+  let r = db.transaction([SBG_DB_NAME_TABLE], 'readwrite')
+    .objectStore(SBG_DB_NAME_TABLE)
+    .get(1);
+
+  return new Promise((resolve, reject) => {
+    r.onsuccess = function (event) {
+      resolve(r.result)
+    };
+
+    r.onerror = function (event) {
+      reject(event)
+    }
+  })
+}
+
+async function uploadImgChange(e) {
   try {
-    if(e.target.files.length === 0) return;
+    if (e.target.files.length === 0) return;
     const file = e.target.files[0];
-    const img = await convertBase64(file);
-    await setImg(img)
-    setBodyBg(img)
+    await setImg(file)
+    setBodyBg(file)
   } catch (error) {
     console.log('choose file error')
     console.log(error)
   }
 }
 
-const convertBase64 = (file) => {
-  return new Promise((resolve, reject) => {
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(file);
-
-      fileReader.onload = () => {
-          resolve(fileReader.result);
-      };
-
-      fileReader.onerror = (error) => {
-          reject(error);
-      };
-  });
-};
-
-function setBodyBg(url) {
-  if(!url) return;
-  document.body.style.backgroundImage = `url(${url})`;
+function setBodyBg(file) {
+  if (!file) return;
+  const img = URL.createObjectURL(file);
+  document.body.style.backgroundImage = `url(${img})`;
 }
 
-function setImg(img){
-  return chrome.storage.local.set({ [SBG_KEY]: img })
-}
 
-function getImg(){
-  return chrome.storage.local.get([SBG_KEY])
-}
+
+
+
 
