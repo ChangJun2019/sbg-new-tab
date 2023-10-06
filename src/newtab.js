4f66a2fc02f5807c27a13396e0fe89a7ceda92f8
Chang Jun <52chinaweb@gmail.com>
2023-10-06 18:20:35 +0800

    chore: add eslint fix

--- src/newtab.js
+++ src/newtab.js
@@ -1,52 +1,47 @@
-const SBG_KEY = 'EXTEMSIONS-SBG-KEY';
 const SBG_DB_NAME = 'start-db'
 const SBG_DB_NAME_TABLE = 'bg'
-let db = null;
+let db = null
 init()
 
-const uploadImg = document.getElementById('upload-img');
-uploadImg.addEventListener('change', uploadImgChange);
+const uploadImg = document.getElementById('upload-img')
+uploadImg.addEventListener('change', uploadImgChange)
 
 async function init() {
-  await initStore();
+  await initStore()
   const file = await getImg()
   setBodyBg(file)
 }
 
 function initStore() {
-  const request = window.indexedDB.open(SBG_DB_NAME, 1);
+  const request = window.indexedDB.open(SBG_DB_NAME, 1)
 
   return new Promise((resolve, reject) => {
     request.onerror = function (event) {
-      console.log('db open error');
       reject(event)
-    };
+    }
 
     request.onsuccess = function (event) {
-      db = request.result;
+      db = request.result
       resolve(event)
-    };
+    }
 
     request.onupgradeneeded = function (event) {
-      db = event.target.result;
-      let objectStore;
-
-      if (!db.objectStoreNames.contains(SBG_DB_NAME_TABLE)) {
-        objectStore = db.createObjectStore(SBG_DB_NAME_TABLE, { autoIncrement: true });
-      }
+      db = event.target.result
+      if (!db.objectStoreNames.contains(SBG_DB_NAME_TABLE))
+        db.createObjectStore(SBG_DB_NAME_TABLE, { autoIncrement: true })
     }
   })
 }
 
 function setImg(file) {
-  let r = db.transaction([SBG_DB_NAME_TABLE], 'readwrite')
+  const r = db.transaction([SBG_DB_NAME_TABLE], 'readwrite')
     .objectStore(SBG_DB_NAME_TABLE)
-    .put(file, 1);
+    .put(file, 1)
 
   return new Promise((resolve, reject) => {
     r.onsuccess = function (event) {
       resolve(event)
-    };
+    }
 
     r.onerror = function (event) {
       reject(event)
@@ -55,14 +50,14 @@ function setImg(file) {
 }
 
 function getImg() {
-  let r = db.transaction([SBG_DB_NAME_TABLE], 'readwrite')
+  const r = db.transaction([SBG_DB_NAME_TABLE], 'readwrite')
     .objectStore(SBG_DB_NAME_TABLE)
-    .get(1);
+    .get(1)
 
   return new Promise((resolve, reject) => {
-    r.onsuccess = function (event) {
+    r.onsuccess = function () {
       resolve(r.result)
-    };
+    }
 
     r.onerror = function (event) {
       reject(event)
@@ -71,25 +66,16 @@ function getImg() {
 }
 
 async function uploadImgChange(e) {
-  try {
-    if (e.target.files.length === 0) return;
-    const file = e.target.files[0];
-    await setImg(file)
-    setBodyBg(file)
-  } catch (error) {
-    console.log('choose file error')
-    console.log(error)
-  }
+  const f = e.target.files
+  if (f.length === 0)
+    return
+  await setImg(f[0])
+  setBodyBg(f[0])
 }
 
 function setBodyBg(file) {
-  if (!file) return;
-  const img = URL.createObjectURL(file);
-  document.body.style.backgroundImage = `url(${img})`;
+  if (!file)
+    return
+  const img = URL.createObjectURL(file)
+  document.body.style.backgroundImage = `url(${img})`
 }
-
-
-
-
-
-
